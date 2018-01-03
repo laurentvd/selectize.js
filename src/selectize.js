d0e23ad86271c9e82beb6a47aabb5bde8fc998af
Laurent van Dommelen <laurent@studiosugarfree.com>
2018-01-03 14:31:53 +0100

    Fixes closing of dropdown while scrolling in IE11

--- src/selectize.js
+++ src/selectize.js
@@ -627,6 +627,10 @@ $.extend(Selectize.prototype, {
 	 */
 	onBlur: function(e, dest) {
 		var self = this;
+		if (e && e.target === self.$control_input[0]) {
+			return;
+		}
+
 		if (!self.isFocused) return;
 		self.isFocused = false;
 
